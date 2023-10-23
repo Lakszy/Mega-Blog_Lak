905374630a3b99d8856cd138c6ddf69b066c319c
Lakshay Khattar <lakshaykhattar0208@gmail.com>
2023-10-23 11:59:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import conf from './conf/conf'
+function App() {
+  console.log(conf);
+  return (
+    <h1>A Blog With APPWRITE</h1>
+  )
+}
+
+export default App
---
+++ src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+};
+
+export default conf;
